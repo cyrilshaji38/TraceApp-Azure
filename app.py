46c71cb777853f6a4305813c1057d7d9ab130d9d
Cyril Shaji <cyrilshaji38@gmail.com>
2021-06-23 01:53:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
 from azure.storage.queue import (
         QueueClient,
         BinaryBase64EncodePolicy,
@@ -6,16 +9,13 @@ from azure.storage.queue import (
 
 import os, uuid
 
-from flask import Flask, render_template
-app = Flask(__name__)
-
 @app.route("/")
 def home_page(): 
         return render_template('home.html') 
 
 
 connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-q_name = "new-feedback-q" + str(uuid.uuid4())
+q_name = "new" + str(uuid.uuid4())
 print("Creating queue: " + q_name)
 queue_client = QueueClient.from_connection_string(connect_str, q_name)
 queue_client.create_queue()
