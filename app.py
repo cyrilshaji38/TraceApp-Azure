65a2d74ab27d1ec6ec554a0ce5278a4ea5bd39f0
Cyril Shaji <cyrilshaji38@gmail.com>
2021-06-23 02:48:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home_page(): 
         return render_template('home.html') 
 
-# from azure.storage.queue import QueueClient
+from azure.storage.queue import QueueClient
 # connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 # queue_client = QueueClient.from_connection_string(connect_str, "new_queue")
 # queue_client.create_queue()
