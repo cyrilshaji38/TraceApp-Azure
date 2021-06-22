bd8fb1dc641fe89514740fb16efde3a3c2c44638
Cyril Shaji <cyrilshaji38@gmail.com>
2021-06-23 00:06:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+from azure.storage.queue import (
+        QueueService,
+        QueueMessageFormat
+)
+import os, uuid
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to the Trace App! We will help you sort out your product reviews."
+    return '<h1>Welcome to the Trace App! We will help you sort out your product reviews.</h1><br><br><label for="fname">Write a review: </label><input type="text" id="fname" name="fname"><br><br><input type="submit" value="Check Sentiment">'
+      
+  
+  
+
+# message = u"Hello, World"
+# print("Adding message: " + message)
+# QueueService.put_message(new-feedback-q, message)
