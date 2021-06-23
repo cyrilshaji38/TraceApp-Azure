38dc3f248fa5b45813b603401a2e07cccb04cab9
Cyril Shaji <cyrilshaji38@gmail.com>
2021-06-23 13:06:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 def home_page(): 
         return render_template('home.html') 
 
-from azure.storage.queue import QueueServiceClient
-service = QueueServiceClient.from_connection_string(conn_str=connect_str)
-queue = service.get_queue_client(queue="new-feedback-q")
-# from azure.storage.queue import QueueClient
-# queue = QueueClient.from_connection_string(conn_str=connect_str, queue_name="new-feedback-q")
-queue.send_message("very nice food!")
+def add_to_queue():
+        from azure.storage.queue import QueueServiceClient
+        service = QueueServiceClient.from_connection_string(conn_str=connect_str)
+        queue = service.get_queue_client(queue="new-feedback-q")
+        # from azure.storage.queue import QueueClient
+        # # queue = QueueClient.from_connection_string(conn_str=connect_str, queue_name="new-feedback-q")
+        queue.send_message("very nice food!")
+
+if __name__ == '__main__':
+        add_to_queue()
