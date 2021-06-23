4cdfcbdd2b832db7cfdcfce03006cca654954b13
Cyril Shaji <cyrilshaji38@gmail.com>
2021-06-23 21:29:55 +0530

    dependencies fixed

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-from azure.storage import queue
 from flask import Flask, render_template
-import os
+from azure.storage.queue import QueueServiceClient
 
+connect_str = "DefaultEndpointsProtocol=https;AccountName=storageaccounttrace8774;AccountKey=TApeXC/1SCYVXPYZRywFqleViIkAGMpWV1G51D1cVTyjJutNcgjlDSCSW2MzBW6guPS6FUtzD4Y7Yo9c37WZ2w==;EndpointSuffix=core.windows.net"
 app = Flask(__name__)
-connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 
 @app.route("/")
 def home_page(): 
         return render_template('home.html') 
 
-def add_to_queue():
-        from azure.storage.queue import QueueServiceClient
-        service = QueueServiceClient.from_connection_string(conn_str=connect_str)
-        queue = service.get_queue_client(queue="new-feedback-q")
-        # from azure.storage.queue import QueueClient
-        # # queue = QueueClient.from_connection_string(conn_str=connect_str, queue_name="new-feedback-q")
-        queue.send_message("very nice food!")
+class ReviewQueue(object):
+        def add_to_queue():
+                service = QueueServiceClient.from_connection_string(conn_str=connect_str)
+                reviewqueue = service.get_queue_client(queue="new-feedback-q")
+                reviewqueue.send_message("very nice food!")
 
 if __name__ == '__main__':
-        add_to_queue()
+        sample = ReviewQueue()
+        sample.add_to_queue()
