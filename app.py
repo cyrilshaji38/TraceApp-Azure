a9cb5605efd6ccb9a9418a4edcbf51e78e458d82
Cyril Shaji <cyrilshaji38@gmail.com>
2021-06-23 12:08:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 import os
+
 app = Flask(__name__)
+connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 
 @app.route("/")
 def home_page(): 
         return render_template('home.html') 
 
-from azure.storage.queue import QueueClient
-# connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-# queue_client = QueueClient.from_connection_string(connect_str, "new_queue")
+def create_client_with_connection_string(self):
+        from azure.storage.queue import QueueServiceClient
+        queue_service = QueueServiceClient.from_connection_string(conn_str=self.connection_string)
 # queue_client.create_queue()
 # queue_client.send_message(u"Nice food")
