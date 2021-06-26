a00c5b4bddce7158820e477b15d6c747aab521e1
Cyril Shaji <cyrilshaji38@gmail.com>
2021-06-27 00:23:40 +0530

    v3

---
+++ amazon-reviews.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_review(pages):
+
+    
+
+
+    headers = {
+        'authority': 'www.amazon.com',
+        'cache-control': 'max-age=0',
+        'rtt': '100',
+        'downlink': '9.2',
+        'ect': '4g',
+        'sec-ch-ua': '^\\^',
+        'sec-ch-ua-mobile': '?0',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'accept-language': 'en-US,en;q=0.9',
+        'cookie': 'session-id=140-4142375-4005405; i18n-prefs=USD; sp-cdn=^\\^L5Z9:IN^\\^; ubid-main=133-1703036-0414721; s_fid=15ACC4BA280D4F14-038C9A8E5E877985; regStatus=pre-register; aws-target-data=^%^7B^%^22support^%^22^%^3A^%^221^%^22^%^7D; aws-target-visitor-id=1623992980699-29478.31_0; lc-main=en_US; session-id-time=2082787201l; session-token=3C7FE5erzwisM4Q9VFUHzXSA/eMeK109XvSk3qGH0QACt4108/s0lKTOpDVQTEBs90M2tWq4niv7M6Qb/uP3k9iBEKg1xKTsaibq9ACUJiyvDiduYSi6Lo1A5rR3BFtb0hKq5YaiuVdgb0SctM4i6hzPr5w4P3scMT7tePHGZFUKSnw7eYAlsAD5i14Zhu7U; csm-hit=adb:adblk_no&t:1624715411612&tb:3Z2570B9GG7KJPY3JW0K+s-3Z2570B9GG7KJPY3JW0K^|1624715411612',
+    }
+
+    resp = requests.get(pages[0], headers=headers)
+    resp
+
+    soup = BeautifulSoup(resp.text, "html.parser") # html.parser, lxml
+
+    # 10 divs
+    reviews = soup.find_all("div", {"class": "a-section review aok-relative"})
+
+    def get_review_stars(soup_obj: BeautifulSoup) -> str:
+        try:
+            review_str = soup_obj.find('a', {"class": "a-link-normal"}).get_text()
+            return review_str
+        except Exception as e:
+            return 'no_stars'
+            print(e)
+
+    def get_review_body(soup_obj: BeautifulSoup) -> str:
+        try:
+            review_body = soup_obj.find('span', 
+                {"class": "a-size-base review-text review-text-content"}
+            ).get_text().strip()
+            return review_body
+        except Exception as e:
+            return 'no_body'
+            print(e)
+
+    records = [get_review_body(rev) for rev in reviews]
+    return records
+
+
+pages = [
+        "https://www.amazon.com/Heat-Storm-HS-1500-PHX-WIFI-Infrared-Heater/product-reviews/B07JXRWJ8D/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews",
+        "https://www.amazon.com/Heat-Storm-HS-1500-PHX-WIFI-Infrared-Heater/product-reviews/B07JXRWJ8D/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber=2",
+        "https://www.amazon.com/Heat-Storm-HS-1500-PHX-WIFI-Infrared-Heater/product-reviews/B07JXRWJ8D/ref=cm_cr_getr_d_paging_btm_next_3?ie=UTF8&reviewerType=all_reviews&pageNumber=3"
+    ]
+
+print(get_review(pages))
--- app.py
+++ app.py
@@ -1,39 +1,76 @@
-from flask import Flask, render_template
-from azure.storage.queue import (
-        QueueClient,
-        TextBase64EncodePolicy
-)
+from flask import Flask, render_template, redirect, url_for
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 import os
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
-connect_str = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
+subscription_key = os.environ["TEXT_ANALYTICS_SUBSCRIPTION_KEY"]
+endpoint = os.environ["TEXT_ANALYTICS_ENDPOINT"]
+
+def authenticate_client():
+    ta_credential = AzureKeyCredential(subscription_key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=endpoint, 
+            credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+def sentiment_analysis_example(client):
+    r1 = ReviewForm()
+    documents = [r1.review.data]
+    response = client.analyze_sentiment(documents=documents)[0]
+    r2.sentimental_analysis = response.sentiment
+    print("Document Sentiment: {}".format(response.sentiment))
+
+def key_phrase_extraction_example(client):
+    r1 = ReviewForm()
+     
+    try:
+        documents = [r1.review.data]
+        response = client.extract_key_phrases(documents = documents)[0]
+        if not response.is_error:
+            print("\tKey Phrases:")
+            for phrase in response.key_phrases:
+                r2.key_phrases.append(phrase)
+                print("\t\t", phrase)
+        else:
+            print(response.id, response.error)
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '7b040a256ba53639fe34e81ccba6bb41'
 
 @app.route("/", methods = ['GET','POST'])
 def home_page():
-        form = ReviewForm()
-        if form.validate_on_submit():
-                sample = ReviewQueue()
-                sample.add_to_queue()
-        return render_template('home.html', form=form)
+    form = ReviewForm()
+    if form.validate_on_submit():
+        sentiment_analysis_example(client)
+        key_phrase_extraction_example(client)
+        r2.user_review = form.review.data
+        return redirect(url_for('result_page'))
+    return render_template('home.html', form=form)
+
+@app.route("/results")
+def result_page():
+    return render_template('results.html', d1 = r2.user_review, d2 = r2.sentimental_analysis, d3 = r2.key_phrases)
+
 
 class ReviewForm(FlaskForm):
     review = StringField(label='Write a review: ')
-    submit = SubmitField(label='Check Sentiment')
-    
+    submit = SubmitField(label='Check Sentiment')   
 
-class ReviewQueue(object):
-        def add_to_queue(self):
-                reviewqueue = QueueClient.from_connection_string(
-                        conn_str=connect_str, 
-                        queue_name = "new-feedback-q", 
-                        message_encode_policy = TextBase64EncodePolicy()
-                )
-                r1 = ReviewForm()
-                reviewqueue.send_message(r1.review.data)
+class Results():
+    user_review = ""
+    sentimental_analysis = "" 
+    key_phrases = []
+    
+r2 = Results()
 
 
 if __name__ == '__main__':
